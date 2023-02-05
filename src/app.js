cafaba838d3c08cf1c8e70e3f2a32f5beec4755a
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-02-05 20:31:42 +0700

    using view engine and static folder with absolute path

--- src/app.js
+++ src/app.js
@@ -5,14 +5,17 @@ const app = express();
 app.listen(3000);
 
 // middleware
+const static = app.use(express.static(`${__dirname}/public`));
 
 // connect to db
 
 // view engine
+app.set('view engine', 'ejs');
+app.set('views', `${__dirname}/views`);
 
 // routes
 app.use(authRoutes);
 
 app.get('/', (req, res) => {
-  res.send('test');
+  res.render('home');
 });
